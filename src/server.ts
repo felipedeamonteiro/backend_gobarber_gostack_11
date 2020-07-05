5506c952dade10edb96637a72a2e55a25e7888d0
Felipe Monteiro <felipedeamonteiro@gmail.com>
2020-07-05 00:50:52 -0300

    Add images upload feature

--- src/server.ts
+++ src/server.ts
@@ -9,12 +9,14 @@ import express from 'express';
 // com erros, o vscode cuida disso
 // A flag '--inspect' serve para o debug do vscode se conectar à aplicação
 import routes from './routes';
+import uploadConfig from './config/upload';
 
 import './database';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
