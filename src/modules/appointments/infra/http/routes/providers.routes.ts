3205b10a40dfba862a1de2303713753ffd2d72c7
Felipe Monteiro <felipedeamonteiro@gmail.com>
2020-08-24 01:17:17 -0300

    Add controllers and routes

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -3,12 +3,24 @@ import { Router } from 'express';
 
 import ensureAutheticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProvidersController from '../controllers/ProvidersController';
+import ProviderMonthAvailabilityController from '../controllers/ProviderMonthAvailabilityController';
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController';
 
 const providerRouter = Router();
 const providersController = new ProvidersController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
 
 providerRouter.use(ensureAutheticated);
 
 providerRouter.get('/', providersController.index);
+providerRouter.get(
+  '/:provider_id/month-availability',
+  providerMonthAvailabilityController.index,
+);
+providerRouter.get(
+  '/:provider_id/day-availability',
+  providerDayAvailabilityController.index,
+);
 
 export default providerRouter;
