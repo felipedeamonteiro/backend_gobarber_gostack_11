c3a5864b947ffd7e15d7db29d9880a844418f15f
Felipe Monteiro <felipedeamonteiro@gmail.com>
2020-08-26 03:35:49 -0300

    Add fake notification repository to tests

---
+++ src/modules/notifications/repositories/fakes/FakeNotificationsRepository.ts
@@ -0,0 +1,29 @@
+/* eslint-disable camelcase */
+import { ObjectID } from 'mongodb';
+
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
+import ICreateNotificationDTO from '@modules/notifications/dtos/ICreateNotificationDTO';
+
+import Notification from '../../infra/typeorm/schemas/Notification';
+
+// DTO = Data Transfer Object => Basicamente é a transferência de dados em formato
+// de objetos
+
+class NotificationsRepository implements INotificationsRepository {
+  private notifications: Notification[] = [];
+
+  public async create({
+    content,
+    recipient_id,
+  }: ICreateNotificationDTO): Promise<Notification> {
+    const notification = new Notification();
+
+    Object.assign(notification, { id: new ObjectID(), content, recipient_id });
+
+    this.notifications.push(notification);
+
+    return notification;
+  }
+}
+
+export default NotificationsRepository;
