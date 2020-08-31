c49721d00bac3d05f5fc7cc1141f5cc2b9beaa21
Felipe Monteiro <felipedeamonteiro@gmail.com>
2020-08-31 03:16:14 -0300

    Add express limit rate to avoid brute force

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -15,6 +15,7 @@ import 'express-async-errors';
 // A flag '--inspect' serve para o debug do vscode se conectar à aplicação
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
+import rateLimiter from './middlewares/rateLimiter';
 import routes from './routes';
 
 import '@shared/infra/typeorm';
@@ -22,6 +23,7 @@ import '@shared/container';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
