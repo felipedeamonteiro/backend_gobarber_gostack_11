d946c2291d6a74155e1c7e3b8c23506e4c6bb5c7
Felipe Monteiro <felipedeamonteiro@gmail.com>
2020-09-08 00:30:18 -0300

    Fix db relation between appointments and users

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -44,7 +44,6 @@ class AppointmentsRepository implements IAppointmentsRepository {
             `to_char(${dateFieldName}, 'MM-YYYY') = '${parsedMonth}-${year}'`,
         ),
       },
-      relations: ['user'],
     });
 
     return appointment;
